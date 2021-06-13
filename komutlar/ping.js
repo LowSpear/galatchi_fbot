39f5170d40fb1eeb20e72a8877f6c8115aaf8dd8
LowSpear <73055590+LowSpear@users.noreply.github.com>
2021-06-13 13:34:22 +0300

    Update ping.js

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -1,15 +1,8 @@
 const Discord = require('discord.js');
 
-exports.run = async (client, message, args,) => {
-  const useruser = "Komut "  + message.author.username + " tarafından çalıştırıldı";
-  const userurl = message.author.avatarURL;
-  const bayrak = ":flag_tr:";
-  const ping = `${Math.round(client.ping)}ms`;
-
 "Mesaj Gecikmesi: {ping1} ms \nBot Gecikmesi: {ping2} ms"
         .replace("{ping1}", new Date().getTime() - message.createdTimestamp)
         .replace("{ping2}", client.ws.ping),true)
-
 }
 
 exports.conf = {
